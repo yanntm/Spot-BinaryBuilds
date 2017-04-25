31c7bb4e15dee2c1a8f95973f8e529eabbab8d9a
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-25 14:24:30 +0200

    build files, taken from old teamcity or from libits

---
+++ build_spot.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+wget  --no-check-certificate https://teamcity.lrde.epita.fr/guestAuth/repository/download/bt18/.lastSuccessful/spot-snapshot.tar.gz
+
+tar zxvf spot-snapshot.tar.gz
+
+cd spot-*
+./configure -C VALGRIND=false
+
+# CPPFLAGS='-I%system.pkg64.libboost.path%/include' LDFLAGS='-L%system.pkg64.libboost.path%/lib' VALGRIND=false
+
+make -j2
+
+make check -j2
+
+cd ..
+
+mkdir install_dir
+cd install_dir
+export IFOLDER=$(pwd)
+
+cd ../spot-*
+
+make install DESTDIR=$IFOLDER
+
+cd ../install_dir
+
+tar cfvz ../spot_linux_64.tar.gz *
+
+cp spot_linux_64.tar.gz ../website
