1a9da7fc6c45daafa12ca98a65b49e723d570a91
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-05-24 18:28:57 +0200

    supporting windows paths

--- build_spot.sh
+++ build_spot.sh
@@ -5,30 +5,33 @@ wget  --no-check-certificate https://teamcity.lrde.epita.fr/guestAuth/repository
 tar zxf spot-snapshot.tar.gz
 rm spot-snapshot.tar.gz
 
-cd spot-*
-./configure -C VALGRIND=false --without-included-lbtt --disable-devel --disable-shared --disable-python
+mkdir install_dir
+cd install_dir
+export IFOLDER=$(pwd)
+cd ../spot-*
+
+./configure -C VALGRIND=false --without-included-lbtt --disable-devel --disable-shared --disable-python --prefix=$IFOLDER
 
 # CPPFLAGS='-I%system.pkg64.libboost.path%/include' LDFLAGS='-L%system.pkg64.libboost.path%/lib' VALGRIND=false
 
 make -j2
 
 # make check -j2
-
-cd ..
-
-mkdir install_dir
-cd install_dir
-export IFOLDER=$(pwd)
-
-cd ../spot-*
-
-make install DESTDIR=$IFOLDER
+make install 
 
 cd ../install_dir
+
+if [ ! -d usr/local ] ; then 
+    mkdir usr/
+    mkdir usr/local
+    mv mingw64/* usr/local/
+    \rm -r mingw64
+fi
 
 cd usr/local/
 \rm -rf bin/ share/ lib64/
 cd ../..
+
 
 ls -lah
 
