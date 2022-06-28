d10e17149826db38c35f506a7237a244420926bc
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2022-06-28 12:35:19 +0200

    use 2.10.6 (for real)

--- build_spot.sh
+++ build_spot.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-export SPOTVER=2.10.4.dev
+export SPOTVER=2.10.6.dev
 
 #wget --progress=dot:mega --no-check-certificate http://www.lrde.epita.fr/dload/spot/spot-$SPOTVER.tar.gz
-wget --progress=dot:mega --no-check-certificate https://www.lrde.epita.fr/~adl/spot-2.10.6.dev.tar.gz
+wget --progress=dot:mega --no-check-certificate https://www.lrde.epita.fr/~adl/spot-$SPOTVER.tar.gz
 
 tar zxf spot-$SPOTVER.tar.gz
 rm spot-$SPOTVER.tar.gz
