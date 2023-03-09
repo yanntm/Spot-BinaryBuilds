4bb2f3adbb549e14fd1d52237c84d524bc66f125
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-03-09 15:13:52 +0100

    back to using 2.11 now problems downstream are solved

--- build_spot.sh
+++ build_spot.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 # Minato version
-# export SPOTVER=2.11.1.dev
+export SPOTVER=2.11.1.dev
 # restrict/relax version
-export SPOTVER=2.10.4.dev
+# export SPOTVER=2.10.4.dev
 
 
 #wget --progress=dot:mega --no-check-certificate http://www.lrde.epita.fr/dload/spot/spot-$SPOTVER.tar.gz
